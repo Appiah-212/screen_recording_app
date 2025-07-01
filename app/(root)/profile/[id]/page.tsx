f9336b2a92e33297b53b2a0e4c0967bc1f590aa7
Appiah-212 <yawtawia212@gmail.com>
2025-07-01 22:22:33 +0100

    VideoCard UI Implementation

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,4 +1,6 @@
 import Header from "@/components/Header";
+import VideoCard from "@/components/VideoCard";
+import { dummyCards } from "@/constants";
 
 const page = async ({ params }: ParamsWithSearch) => {
   const { id } = await params;  
@@ -6,8 +8,13 @@ const page = async ({ params }: ParamsWithSearch) => {
     <div className="wrapper page">
         <Header subHeader ="kubiman@gmail.com" title="Kubiman | Daakyehene AI" userImg="/assets/images/dummy.jpg" />
 
+        <section className="video-grid">
+          {dummyCards.map((card) => (
+        <VideoCard key={card.id} {... card} />
+      ))}
 
-        <h1 className="text-2xl font-karla">USER ID: {id}</h1>
+        </section>
+         
     </div>
   )
 }
