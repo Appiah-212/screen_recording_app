db0c688be91a3bde18ea59ce53a1d8c0f14120b5
Appiah-212 <yawtawia212@gmail.com>
2025-07-04 17:00:56 +0100

    Database Design

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,12 @@
+'use client';
+import { authClient } from "@/lib/auth-client";
 import Image from "next/image"
 import Link from "next/link"
 
 const page = () => {
+  const handleSignIn = async () => {
+    return await authClient.signIn.social({ provider: 'google'})
+  }
   return (
     <main className="sign-in">
       <aside className='testimonial'>
@@ -41,7 +46,7 @@ const page = () => {
                <h1>SnapCast</h1>
           </Link>
           <p>Create and share your very first <span>SnapCast video</span> in no time!</p>
-          <button>
+          <button onClick={handleSignIn}>
             <Image src="/assets/icons/google.svg" alt="google" width={22} height={22} />
             <span>Sign in with Google</span>
           </button>
